7ad6a1e7b3d04da28b9a3bdf491ee3cfe6affe5f
Павел Стерехов <iiabejic@gmail.com>
2021-11-03 16:32:55 +0500

    Fixed files

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -68,20 +68,18 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
- {
-  'NAME':
-  'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
- },
-
- {
-  'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
- },
- {
-  'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
- },
- {
-  'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
- },
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 LANGUAGE_CODE = 'ru'
@@ -106,8 +104,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
 }
 
